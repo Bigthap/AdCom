48ee81a6c411052f71176880162d15ea3eb2e5a8
Bigthap <hangdongbigc2@Gmail.com>
2026-01-08 11:35:53 +0700

    Update productPrice.php

--- Ch5/productPrice.php
+++ Ch5/productPrice.php
@@ -28,8 +28,38 @@
     $discountAmount = $cake * $discount;
     $netprice = ($cake - $discountAmount) + $s;
 
-    echo "Total Price: " . number_format($cake) . " THB<br>";
-    echo "Discount: " . ($discount * 100) . "% (" . number_format($discountAmount) . " THB)<br>";
-    echo "Shipping ($ship): $s THB<br>";
-    echo "<b>Net Price: " . number_format($netprice) . " THB</b>";
+    // Output moved to HTML body below
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Save The World: Order Form</title>
+    <link rel="stylesheet" href="product.css?v=<?php echo time(); ?>">
+</head>
+
+<body>
+
+    <div class="header">
+        <img src="Img/Hero2.jpg" alt="Recipe World Logo">
+        <h3>Menu1</h3>
+        <h3>Menu2</h3>
+        <h3>Menu3</h3>
+    </div>
+
+  <div class="content-box">
+        <h2>Order Summary</h2>
+        <p> Total Price : <span style="color: red;"><?php echo "$cake (THB)"?></span><br>
+            Shipment Type : <span style="color: red;"><?php echo "$ship ($s THB)"?></span><br>
+            Discount: <span style="color: red;"><?php echo ($discount * 100) . "% (" . number_format($discountAmount, 2) . " THB)"; ?></span><br>
+            <b>Net Price: <span style="color: red;"><?php echo number_format($netprice, 2); ?></span> THB</b><br>
+            <h3>Thank You Very Much </h3>
+        </p>
+    </div>
+
+</body>
+
+</html>
